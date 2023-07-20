66e4f0f2643b44b64e8d4996ea15f2a41868c14d
Aanand Aman <109314074+Aanand058@users.noreply.github.com>
2023-07-19 23:28:18 -0400

    Add files via upload

---
+++ MS2/Utilities.cpp
@@ -0,0 +1,69 @@
+// Name: Aanand Aman
+// Seneca Student ID: 166125211
+// Seneca email: aaman8@myseneca.ca
+// Date of completion: 2023/03/15
+//
+// I confirm that I am the only author of this file
+//   and the content was created entirely by me.
+
+
+#include "Utilities.h"
+
+using namespace std;
+
+namespace sdds {
+
+
+    char Utilities::m_delimiter;
+
+    void Utilities::setFieldWidth(size_t newWidth)
+    {
+        m_widthField = newWidth;
+    }
+
+
+    size_t Utilities::getFieldWidth() const
+    {
+        return this->m_widthField;
+    }
+
+
+    string Utilities::extractToken(const string& str, size_t& next_pos, bool& more)
+    {
+
+        size_t pos = str.find(m_delimiter, next_pos);
+        if (pos == next_pos)
+        {
+            more = false;
+            throw invalid_argument("Invalid pos");
+        }
+
+        string line = str.substr(next_pos, pos - next_pos);
+
+        trim(line);
+
+        m_widthField = max(line.length(), getFieldWidth());
+
+        if (pos != string::npos)
+        {
+            next_pos = pos + 1;
+            more = true;
+        }
+        else {
+            more = false;
+        }
+        return line;
+    }
+
+
+    void Utilities::setDelimiter(char newDelimiter)
+    {
+        m_delimiter = newDelimiter;
+    }
+
+
+    char Utilities::getDelimiter()
+    {
+        return m_delimiter;
+    }
+}
